2664b59d4e317ada5416d47a1897cd0b21033c3a
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2024-12-30 09:51:06 +0530

    feat: :zap: Add State Management

--- currencyConverter/app/index.tsx
+++ currencyConverter/app/index.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 
 const CurrencyConverterApp = () => {
+    const [amount, setAmount] = useState<string>(''); // Specify type as string
+    const [fromCurrency, setFromCurrency] = useState<string>('USD');
+    const [toCurrency, setToCurrency] = useState<string>('EUR');
+    const [result, setResult] = useState<string | null>(null); // Allow result to be string or null
+
     return (
         <View>
             <Text>Currency Converter</Text>
